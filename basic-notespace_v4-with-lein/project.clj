ab42090cf03e35aa1dda8a02e373dab1d1734e94
daslu <daniel.slutsky@gmail.com>
2021-10-14 02:23:39 +0300

    updated demo

--- basic-notespace_v4-with-lein/project.clj
+++ basic-notespace_v4-with-lein/project.clj
@@ -1,8 +1,8 @@
 (defproject basic-notespace_v4-with-lein "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Using Notespace v4 in a Leiningen project"
+  :url "https://github.com/scicloj/notespace-examples/tree/main/basic-notespace_v4-with-lein"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.3"]
-                 [scicloj/notespace "4-alpha-3"] ]
+                 [scicloj/notespace "4-alpha-4"] ]
   :repl-options {:nrepl-middleware [scicloj.notespace.v4.nrepl/middleware]})
--- basic-notespace_v4-with-lein/src/basic_notespace_v4_with_lein/demo.clj
+++ basic-notespace_v4-with-lein/src/basic_notespace_v4_with_lein/demo.clj
@@ -1,8 +1,15 @@
+; # Notespace v4 tutorial
+
+; ## Setup
+
 (ns basic-notespace-v4-with-lein.demo
-  (:require [scicloj.notespace.v4.api :as notespace]))
+  (:require [scicloj.notespace.v4.api :as notespace]
+            [scicloj.kindly.api :as kindly]
+            [scicloj.kindly.kind :as kind]))
+
+;; To start (or restart) Notespace, use the following call:
 
 (comment
-  (notespace/restart! {})
-  (notespace/restart-events!))
+  (notespace/restart!))
 
 (+ 1 2)
